a7829ed50d6ca02bfa79828fc5a77365665b2748
Somesh Nukala <n.someshmca@gmail.com>
2017-11-26 15:14:05 +0530

    banner slides update and pattern update

--- js/bpjs.js
+++ js/bpjs.js
@@ -5,24 +5,33 @@ $(document).ready(function() {
 		loop:true,
 		items:1,
 		nav:false,
-		dots:false
+		dots:false,
+		autoplay:false,
+		autoplayTimeout:5000,
+		autoplayHoverPause:true
 	}); 
-	 
+	
+	 $(".trigger").click(function() {
+		$(".banner3RadialMenu").toggleClass("active"); 
+	  });
+	
 	$('.homeImagesCarousel').owlCarousel({
-    loop:true,
-    nav:true,
-	dots:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        800:{
-            items:3
-        }
-    },
-		navText : ["<i class='fa fa-chevron-left fa-4x'></i>","<i class='fa fa-chevron-right fa-4x'></i>"]
+		loop:true,
+		nav:true,
+		dots:false,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:2
+			},
+			800:{
+				items:3
+			}
+		},
+			navText : ["<i class='fa fa-chevron-left fa-4x'></i>","<i class='fa fa-chevron-right fa-4x'></i>"]
 	});
+	
+	
 });
